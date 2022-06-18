83c73d2ad51f07003fbd53f2bb16adc0d3b1c345
FAYTOUT <achraffaytout1@gmail.com>
2022-06-18 16:51:10 +0100

    error_forcasting_X8_X14_Solved

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-filename = "Models/modelSVM-StdPerformance.sav" # Model Random Forest regressor
+filename = "Models/modelRFC-StdPerformance.sav" # Model SVM classifier
 model = pickle.load(open(filename,'rb'))
 
 @app.route("/")
@@ -14,36 +14,27 @@ def home():
 
 @app.route("/predict",methods=['GET','POST'])
 def predict():
-    features = [float(x) for x in request.form.values()]
+    features = [int(x) for x in request.form.values()]
+    print('first declared features : ', features)
 
     lis_state=[]
     lis_parent=[]
-    if(features[-2]==0):
-        lis_state=[1,0,0,0,0]
-    elif(features[-2]==1):
-        lis_state=[0,1,0,0,0]
-    elif(features[-2]==2):
-        lis_state=[0,0,1,0,0]
-    elif(features[-2]==3):
-        lis_state=[0,0,0,1,0]
-    elif(features[-2]==4):
-        lis_state=[0,0,0,0,1]
+    if(features[-2]==0): lis_state=[1,0,0,0,0]
+    elif(features[-2]==1): lis_state=[0,1,0,0,0]
+    elif(features[-2]==2): lis_state=[0,0,1,0,0]
+    elif(features[-2]==3): lis_state=[0,0,0,1,0]
+    elif(features[-2]==4): lis_state=[0,0,0,0,1]
 
-    if(features[-1]==0):
-        lis_parent=[1,0,0,0,0,0]
-    elif(features[-2]==1):
-        lis_parent=[0,1,0,0,0,0]
-    elif(features[-2]==2):
-        lis_parent=[0,0,1,0,0,0]
-    elif(features[-2]==3):
-        lis_parent=[0,0,0,1,0,0]
-    elif(features[-2]==4):
-        lis_parent=[0,0,0,0,1,0]
-    elif(features[-2]==5):
-        lis_parent=[0,0,0,0,0,1]
+    if(features[-2]==0): lis_parent=[1,0,0,0,0,0]
+    elif(features[-2]==1): lis_parent=[0,1,0,0,0,0]
+    elif(features[-2]==2): lis_parent=[0,0,1,0,0,0]
+    elif(features[-2]==3): lis_parent=[0,0,0,1,0,0]
+    elif(features[-2]==4): lis_parent=[0,0,0,0,1,0]
+    elif(features[-2]==5): lis_parent=[0,0,0,0,0,1]
 
     features = features[:3] + lis_parent + lis_state
-
+    print('features : ', features)
+    print('lis_state : ', lis_state)
     f_features = [np.array(features)]
     prediction = model.predict(f_features)
 
